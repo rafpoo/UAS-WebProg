1e59bab4247dd1e6d935be445b45e9995a53a4b6
Herqz <pilrealthegunner@gmail.com>
2024-12-10 21:25:49 +0700

    Update aktivitas.blade.php

--- resources/views/user/aktivitas.blade.php
+++ resources/views/user/aktivitas.blade.php
@@ -6,11 +6,9 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> 
         <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&amp;family=Roboto:wght@400;700&amp;display=swap" rel="stylesheet"/>
-        <style>
-    .accordion-body {
-        color: #000; 
-        }
-</style>
+
+        @include('global_css.css')
+        @include('css_in_view.aktivitas_css')
 
     </head>
     <body>
