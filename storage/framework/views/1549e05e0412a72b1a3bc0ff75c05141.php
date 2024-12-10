483045521052d21c6c80256a0d25f36deb80c593
Aclek <rachen.ziyad@student.umn.ac.id>
2024-12-11 00:52:57 +0700

    update tentang kami

---
+++ storage/framework/views/1549e05e0412a72b1a3bc0ff75c05141.php
@@ -0,0 +1,6 @@
+<style>
+    .accordion-body {
+        color: #000; 
+    }
+</style>
+<?php /**PATH C:\Users\ziyad\Downloads\uaswebprog\UAS-WebProg\resources\views/css_in_view/aktivitas_css.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e88a4312ce0737df0ecca0fd57746f28.php
@@ -0,0 +1,2 @@
+
+<?php /**PATH C:\Users\ziyad\Downloads\uaswebprog\UAS-WebProg\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
